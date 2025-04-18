4e2085846f7f7371fe33761be24f9caa34197fd5
kylechoi101 <59933377+kylechoi101@users.noreply.github.com>
2025-04-18 12:52:44 -0700

    jason

--- global.js
+++ global.js
@@ -3,10 +3,40 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-const navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+
+// const navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'reume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'github/', title: 'Github' },
+  ];
+
+const BASE_PATH =
+  location.hostname === "localhost" ||
+  location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
+const nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let href = p.url;
+    if (!href.startsWith("http")) {
+      href = BASE_PATH + href;
+    }
   
-currentLink?.classList.add('current');
+    // inject the anchor
+    nav.insertAdjacentHTML(
+      "beforeend",
+      `<a href="${href}">${p.title}</a>`
+    );
+  }
