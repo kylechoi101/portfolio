044ef8a519efa78550010b0eff2d587ee0e771b9
kylechoi101 <59933377+kylechoi101@users.noreply.github.com>
2025-04-25 15:13:51 -0700

    jason

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 const titleEl = document.querySelector('.projects-title');
 if (titleEl) {
-  titleEl.textContent = `(${projects.length}) Projects`;
+  titleEl.textContent = `${projects.length} Projects`;
 }
 
 renderProjects(projects, projectsContainer, 'h2');
